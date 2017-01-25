fcb5120ce564506890db48a5aafc854135e51214
tlambrou <lambrou@gmail.com>
2017-01-25 13:42:05 -0800

    Updates

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
+// Declarations
 var express = require('express')
 var app = express()
 
+// Middleware
+app.use(express.static('public'))
+app.use(express.static('files'))
+
+// Routing
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
@@ -17,6 +23,7 @@ app.delete('/user', function (req, res) {
   res.send('Got a DELETE request at /user')
 })
 
+// Server
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
