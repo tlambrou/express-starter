b9983aafe18bae7fa4c1701e7b7ed4059c09752f
tlambrou <lambrou@gmail.com>
2017-01-27 14:19:22 -0800

    Initial working app

--- app.js
+++ app.js
@@ -1,35 +1,33 @@
 // Declarations
 var express = require('express');
 var exphbs  = require('express-handlebars');
-
 var app = express();
 
-
-
 // Middleware
-app.use(express.static('public'))
-app.use(express.static('files'))
+app.use(express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // Routing
 app.get('/', function (req, res) {
-    res.render('home');
+    res.render('main');
+    res.send('<h1>I\'m Alive!</h1>');
 });
 
 app.post('/', function (req, res) {
-  res.send('Got a POST request')
-})
+  res.send('Got a POST request');
+});
 
 app.put('/user', function (req, res) {
-  res.send('Got a PUT request at /user')
-})
+  res.send('Got a PUT request at /user');
+});
 
 app.delete('/user', function (req, res) {
-  res.send('Got a DELETE request at /user')
-})
+  res.send('Got a DELETE request at /user');
+});
 
 // Server
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Example app listening on port 3000!');
+});
