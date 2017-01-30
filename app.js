a3275be29004247782d483a121ba248c3fc74f54
tlambrou <lambrou@gmail.com>
2017-01-30 10:54:51 -0800

    Completed Challenges 1 & 2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
-var posts =   { body: "the stuff" };
+var posts =   [{ title: "10 Ways to Taxidermy Your Pets", category: "lifehacks", content: "Etiam porta sem malesuada magna mollis euismod. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id elit non mi porta gravida at eget metus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et." }, {title: "Science Proves That Nothing Is Real", category: "science", content: "Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Maecenas faucibus mollis interdum."}, {title: "Newly Discovered Records Show that 16th Century Pirates Realized the Real Treasure They Were Looking for Was the Memories They Were Creating All Along", category: "history", content: "Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Curabitur blandit tempus porttitor. Nulla vitae elit libero, a pharetra augue."}];
 
 // Middleware
 app.use(express.static('public'));
@@ -16,7 +16,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/posts', function(req, res) {
-    res.render('posts-index', {posts:posts});
+    res.render('posts-index', { posts: posts});
 });
 
 app.get('/greetings/:name', function(req, res) {
